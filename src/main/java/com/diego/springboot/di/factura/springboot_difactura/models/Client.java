193580799324be8718475c07a033181397701ea6
Diego Fernando Guerrero Perez <diego.guerrero9612@gmail.com>
2024-09-27 11:35:31 -0500

    Agregamos atrivutos a cada uno de los modelos, ademas definimos como componentes a Invoice y Client, De esta manera inyectamos a Client en Invoice

--- src/main/java/com/diego/springboot/di/factura/springboot_difactura/models/Client.java
+++ src/main/java/com/diego/springboot/di/factura/springboot_difactura/models/Client.java
@@ -1,5 +1,27 @@
 package com.diego.springboot.di.factura.springboot_difactura.models;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class Client {
+
+    private String name;
+    private String lastName;
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 
 }
--- src/main/java/com/diego/springboot/di/factura/springboot_difactura/models/Product.java
+++ src/main/java/com/diego/springboot/di/factura/springboot_difactura/models/Product.java
@@ -2,4 +2,23 @@ package com.diego.springboot.di.factura.springboot_difactura.models;
 
 public class Product {
 
+    private String nameProduct;
+    private Integer price;
+
+    public String getNameProduct() {
+        return nameProduct;
+    }
+
+    public void setNameProduct(String nameProduct) {
+        this.nameProduct = nameProduct;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+
 }
